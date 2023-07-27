a997b4e09d99587e1dae4f2c1697c7c4f19911f3
yassine <ouchen606@gmail.com>
2023-07-27 23:21:35 +0100

    change the fontFamily and fix some bugs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,20 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
-			},
+				sans: [
+					'-apple-system',
+					'BlinkMacSystemFont',
+					'Segoe UI',
+					'Roboto',
+					'Helvetica Neue',
+					'Arial',
+					'Noto Sans',
+					'sans-serif',
+					'Apple Color Emoji',
+					'Segoe UI Emoji',
+					'Segoe UI Symbol',
+					'Noto Color Emoji',
+				  ],			},
 		},
 	},
 	plugins: [require("@tailwindcss/typography")],
