17a962ef9ddf9660b4ce9b65f61433e1bf4044bf
Parth V Square <26404957+Parthvsquare@users.noreply.github.com>
2023-10-31 22:28:17 +0530

    fix: footer navigation (#23)
    * fix: footer navigation
    
    * fix: rmv redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,37 +13,48 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.monolithon.com",
-  vite: {
-    define: {
-      __DATE__: `'${new Date().toISOString()}'`
-    }
-  },
-  redirects: {
-    "/statusz": "/hu/statusz",
-    "/uzemeltetes": "/hu/uzemeltetes",
-    "/szamlazas": "/hu/szamlazas",
-    "/konyveles": "/hu/konyveles",
-    "/monolithon": "/hu/monolithon",
-    "/bejelentkezes": "/hu/bejelentkezes",
-    "/szoftverek": "/hu/szoftverek",
-    "/tudasbazis": "/hu/tudasbazis",
-    "/partnerek": "partnerek",
-    "/jogok": "/hu/jogok"
-  },
-  integrations: [react(), tailwind(), sitemap({
-    filter: page => page !== "https://www.monolithon.com/hu/titok/" && page !== "https://www.monolithon.com/secret/"
-  }), robots({
-    sitemap: "https://www.monolithon.com/sitemap-0.xml",
-    policy: [{
-      userAgent: ["*"],
-      allow: ["/"],
-      disallow: ["/titok", "/hu/titok", "/secret", "/hu/secret"]
-    }]
-  }), astroI18next(), alpinejs(), partytown()],
-  markdown: {
-    rehypePlugins: [rehypeSlug,
-    // This adds links to headings
-    [rehypeAutolinkHeadings, autolinkConfig]]
-  }
+	site: "https://www.monolithon.com",
+	vite: {
+		define: {
+			__DATE__: `'${new Date().toISOString()}'`,
+		},
+	},
+	redirects: {
+		"/statusz": "/hu/statusz",
+		"/uzemeltetes": "/hu/uzemeltetes",
+		"/szamlazas": "/hu/szamlazas",
+		"/konyveles": "/hu/konyveles",
+		"/bejelentkezes": "/hu/bejelentkezes",
+		"/szoftverek": "/hu/szoftverek",
+		"/tudasbazis": "/hu/tudasbazis",
+		"/partnerek": "partnerek",
+		"/jogok": "/hu/jogok",
+	},
+	integrations: [
+		react(),
+		tailwind(),
+		sitemap({
+			filter: (page) => page !== "https://www.monolithon.com/hu/titok/" && page !== "https://www.monolithon.com/secret/",
+		}),
+		robots({
+			sitemap: "https://www.monolithon.com/sitemap-0.xml",
+			policy: [
+				{
+					userAgent: ["*"],
+					allow: ["/"],
+					disallow: ["/titok", "/hu/titok", "/secret", "/hu/secret"],
+				},
+			],
+		}),
+		astroI18next(),
+		alpinejs(),
+		partytown(),
+	],
+	markdown: {
+		rehypePlugins: [
+			rehypeSlug,
+			// This adds links to headings
+			[rehypeAutolinkHeadings, autolinkConfig],
+		],
+	},
 });
