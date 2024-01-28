af1afdf03676826cc0c33e39978818a0971d13f0
Parth V <26404957+Parthvsquare@users.noreply.github.com>
2024-01-29 02:07:22 +0530

    feat: added env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
+interface ImportMetaEnv {
+  readonly PRODUCT_ID_1: string;
+  readonly PRODUCT_ID_2: string;
+  readonly PRODUCT_ID_3: string;
+  readonly PADDLE_API_KEY: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
