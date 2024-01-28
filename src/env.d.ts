4f3ecde7cc8b44e7c4a80bdaae580468de22e3eb
Parth V <26404957+Parthvsquare@users.noreply.github.com>
2024-01-29 02:44:28 +0530

    fix: paddle client api key

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,7 +3,8 @@ interface ImportMetaEnv {
   readonly PRODUCT_ID_1: string;
   readonly PRODUCT_ID_2: string;
   readonly PRODUCT_ID_3: string;
-  readonly PADDLE_API_KEY: string;
+  readonly PUBLIC_PADDLE_CLIENT_API_KEY: string;
+  readonly PADDLE_CLIENT_API_KEY: string;
   readonly ENVIRONMENT: string;
 
   // more env variables...
