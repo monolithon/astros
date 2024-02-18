a401a9f2d924abe437922630de47804ae5231ab8
Parth V Square <26404957+Parthvsquare@users.noreply.github.com>
2024-02-18 22:16:32 +0530

    Revert "fix: remove partytown firefox (#36)" (#37)
    This reverts commit 632573352bc4c3f79ca7b593d0bf6b2e93ecfe04.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import { autolinkConfig } from "./plugins/rehype-autolink-config";
+import rehypeSlug from "rehype-slug";
+import astroI18next from "astro-i18next";
 import alpinejs from "@astrojs/alpinejs";
 import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import astroI18next from "astro-i18next";
 import robots from "astro-robots";
-import { defineConfig } from "astro/config";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeSlug from "rehype-slug";
-import { autolinkConfig } from "./plugins/rehype-autolink-config";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -66,6 +68,7 @@ export default defineConfig({
     }),
     astroI18next(),
     alpinejs(),
+    partytown(),
   ],
   markdown: {
     rehypePlugins: [
